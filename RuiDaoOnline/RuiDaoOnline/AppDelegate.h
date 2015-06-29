d665e32bd464262601a5f056832754a73a900854
stu <stu@Lab304deiMac-3.local>
2015-06-29 10:33:59 +0800

    睿道在线App项目create

---
+++ RuiDaoOnline/RuiDaoOnline/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuiDaoOnline
+//
+//  Created by stu on 15/6/29.
+//  Copyright (c) 2015年 YoonHyonSung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
