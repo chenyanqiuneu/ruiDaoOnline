0d2dc03ad7472cb060cf7fe9da00c0ccc90cb416
stu <stu@Lab304deiMac-6.local>
2015-06-29 10:40:30 +0800

    修改1

--- RuiDaoOnline/RuiDaoOnline/ViewController.h
+++ RuiDaoOnline/RuiDaoOnline/ViewController.h
@@ -4,11 +4,12 @@
 //
 //  Created by stu on 15/6/29.
 //  Copyright (c) 2015年 YoonHyonSung. All rights reserved.
-//
+//@j   
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+
 
 
 @end
